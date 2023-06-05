06a9b40ce591e73071d6c50823ce31b9fa6bb1e0
Artur Cozzi <80040130+ArturCozzi@users.noreply.github.com>
2023-06-05 20:16:36 -0300

    criacao do script da base de dados

---
+++ livraria.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE livraria;
+
+USE livraria;
+CREATE TABLE livro(
+	id VARCHAR(200) PRIMARY KEY,
+    titulo VARCHAR(200) NOT NULL,
+    autor VARCHAR(200) NOT NULL,
+    editora VARCHAR(200) NOT NULL,
+    ano INT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL
+);
